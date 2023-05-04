e872b672f44a095ca5aef0b5dde9e1b0beb14ff4
DvaliElgudja <dvali.Elgudja@gmail.com>
2023-05-05 03:20:41 +0400

    add js ~ not fully

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,3 +1,37 @@
-const bttn = document.getElementById('bttn')
+// Access to API /login with POST method
 
-bttn.onclick()
+// Define main variables
+const loginForm = document.getElementById('login-form')
+const userMail = document.getElementById('user-email')
+const userPassword = document.getElementById('user-password')
+
+loginForm.addEventListener('submit', async function(e){
+    e.preventDefault()
+
+    const loginData = {
+        email: userMail.value,
+        password: userPassword.value
+    }
+
+    // Convert the data into JSON
+    const formJSON = JSON.stringify(loginData)
+
+    const response = await fetch('http://localhost:5678/api/users/login', {
+        method: "POST",
+        headers: { 
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: formJSON
+    })
+    const data = await response.json()
+    const tokenId = data.token
+
+    if (response.ok === true){
+        localStorage.setItem('token', tokenId);
+        window.location.href = 'index.html'
+    } else {
+        alert("Erreur dans l'identifiant ou le mot de passe")
+    }
+
+})
